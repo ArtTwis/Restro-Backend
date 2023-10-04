f2e6d96481229549faddd5f7c09a2c965d3ec126
twinkle-goyal <twinkle.goyal@hp.com>
2023-10-04 11:37:09 +0530

    added menuItems

---
+++ MenuItems.js
@@ -0,0 +1,483 @@
+const MenuItems = [
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878118",
+        name: "Ghee Plain Dosa ",
+        category: "Amudham Dosa",
+        description:
+          "Crispy dosa made in pure nandini ghee served with 2 types of chutney and our signature drumstick sambar",
+        imageId: "37b86888e4db77143757a85e13a31147",
+        inStock: 1,
+        isVeg: 1,
+        price: 11900,
+        variants: {},
+        variantsV2: {},
+        addons: [
+          {
+            groupId: "79501432",
+            groupName: "Extra",
+            choices: [
+              {
+                id: "64251265",
+                name: "Chutney",
+                price: 2000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+              {
+                id: "64251266",
+                name: "Sambar",
+                price: 3500,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+            ],
+            maxAddons: -1,
+            maxFreeAddons: -1,
+          },
+        ],
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {
+          text: "Bestseller",
+          textColor: "#ffffff",
+          topBackgroundColor: "#d53d4c",
+          bottomBackgroundColor: "#b02331",
+        },
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        isBestseller: true,
+        ratings: {
+          aggregatedRating: {
+            rating: "4.6",
+            ratingCount: "307 ratings",
+            ratingCountV2: "307",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878119",
+        name: "Ghee Masala Dosa ",
+        category: "Amudham Dosa",
+        description:
+          "Crispy masala dosa made in pure nandini ghee and served with aloo palya, 2 types of chutney and our signature drumstick sambar",
+        imageId: "37b86888e4db77143757a85e13a31147",
+        inStock: 1,
+        isVeg: 1,
+        price: 13900,
+        variants: {},
+        variantsV2: {},
+        addons: [
+          {
+            groupId: "79501433",
+            groupName: "Extra",
+            choices: [
+              {
+                id: "64251265",
+                name: "Chutney",
+                price: 2000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+              {
+                id: "64251266",
+                name: "Sambar",
+                price: 3500,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+            ],
+            maxAddons: -1,
+            maxFreeAddons: -1,
+          },
+        ],
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {
+          text: "Bestseller",
+          textColor: "#ffffff",
+          topBackgroundColor: "#d53d4c",
+          bottomBackgroundColor: "#b02331",
+        },
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        isBestseller: true,
+        ratings: {
+          aggregatedRating: {
+            rating: "4.6",
+            ratingCount: "666 ratings",
+            ratingCountV2: "666",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878120",
+        name: "Ghee Saagu Dosa ",
+        category: "Amudham Dosa",
+        description:
+          "Crispy dosa with vegetable saagu on the inside made in pure nandini ghee served with 2 types of chutney and our signature drumstick sambar",
+        imageId: "37b86888e4db77143757a85e13a31147",
+        inStock: 1,
+        isVeg: 1,
+        price: 13900,
+        variants: {},
+        variantsV2: {},
+        addons: [
+          {
+            groupId: "79501411",
+            groupName: "Extra",
+            choices: [
+              {
+                id: "64251265",
+                name: "Chutney",
+                price: 2000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+            ],
+            maxAddons: -1,
+            maxFreeAddons: -1,
+          },
+        ],
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {},
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        ratings: {
+          aggregatedRating: {
+            rating: "4.7",
+            ratingCount: "47 ratings",
+            ratingCountV2: "47",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878122",
+        name: "Ghee Set Dosa (3 Pcs) ",
+        category: "Amudham Dosa",
+        description:
+          "Fluffy set dosas served with 2 types of chutney and saagu",
+        imageId: "3015578cad0f48dc70b67be39f510d0d",
+        inStock: 1,
+        isVeg: 1,
+        price: 14400,
+        variants: {},
+        variantsV2: {},
+        addons: [
+          {
+            groupId: "79501414",
+            groupName: "Extra",
+            choices: [
+              {
+                id: "64251265",
+                name: "Chutney",
+                price: 2000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+              {
+                id: "64251267",
+                name: "Saagu",
+                price: 4000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+            ],
+            maxAddons: -1,
+            maxFreeAddons: -1,
+          },
+        ],
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {},
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        ratings: {
+          aggregatedRating: {
+            rating: "4.7",
+            ratingCount: "270 ratings",
+            ratingCountV2: "270",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878123",
+        name: "Ghee Tomato Uttapam (2 Pcs) ",
+        category: "Amudham Dosa",
+        description:
+          "Tangy tomato uttapam served with 2 types of chutney & vegetable saagu",
+        imageId: "7837181be29035f9f468f5e06472c2e0",
+        inStock: 1,
+        isVeg: 1,
+        price: 14400,
+        variants: {},
+        variantsV2: {},
+        addons: [
+          {
+            groupId: "79501415",
+            groupName: "Extra",
+            choices: [
+              {
+                id: "64251265",
+                name: "Chutney",
+                price: 2000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+              {
+                id: "64251267",
+                name: "Saagu",
+                price: 4000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+            ],
+            maxAddons: -1,
+            maxFreeAddons: -1,
+          },
+        ],
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {},
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        ratings: {
+          aggregatedRating: {
+            rating: "4.7",
+            ratingCount: "95 ratings",
+            ratingCountV2: "95",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878125",
+        name: "Ghee Ragi Dosa (2 Pcs) ",
+        category: "Amudham Dosa",
+        description:
+          "Healthy option, ragi dosa is served with 2 types of chutney & our signature drumstick sambar",
+        imageId: "8659463507c042b2dd7c2a76920fb667",
+        inStock: 1,
+        isVeg: 1,
+        price: 11900,
+        variants: {},
+        variantsV2: {},
+        addons: [
+          {
+            groupId: "79501416",
+            groupName: "Extra",
+            choices: [
+              {
+                id: "64251265",
+                name: "Chutney",
+                price: 2000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+              {
+                id: "64251267",
+                name: "Saagu",
+                price: 4000,
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+            ],
+            maxAddons: -1,
+            maxFreeAddons: -1,
+          },
+        ],
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {},
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        isGuiltfree: true,
+        ratings: {
+          aggregatedRating: {
+            rating: "4.6",
+            ratingCount: "139 ratings",
+            ratingCountV2: "139",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878130",
+        name: "Ghee Pudi Thatte Idli (1)",
+        category: "Amudham Idli",
+        description:
+          "Melt in the mouth idli topped with pudi and drenched in nandini ghee served with 2 chutneys & our signature drumstick sambar",
+        imageId: "aaaf267e273805115b4eb9e251349d77",
+        inStock: 1,
+        isVeg: 1,
+        price: 9900,
+        variants: {},
+        variantsV2: {},
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {
+          text: "Bestseller",
+          textColor: "#ffffff",
+          topBackgroundColor: "#d53d4c",
+          bottomBackgroundColor: "#b02331",
+        },
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        isBestseller: true,
+        ratings: {
+          aggregatedRating: {
+            rating: "4.7",
+            ratingCount: "738 ratings",
+            ratingCountV2: "738",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878134",
+        name: "Thatte Idli(1)",
+        category: "Amudham Idli",
+        description:
+          "Melt in the mouth rice idli, a classic dish without any garnishing served with 2 types of chutney and our signature drumstick sambar",
+        imageId: "e9337079359d4a5371010b0b4406b7bf",
+        inStock: 1,
+        isVeg: 1,
+        price: 4900,
+        variants: {},
+        variantsV2: {},
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {},
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        ratings: {
+          aggregatedRating: {
+            rating: "4.6",
+            ratingCount: "1180 ratings",
+            ratingCountV2: "1180",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+      info: {
+        id: "107878140",
+        name: "Kesaribath ",
+        category: "Amudham Tiffin Specials",
+        description:
+          "Server 1\na classic dish where kesari refers to the spice saffron which creates the dish's colour.",
+        imageId: "fcdbc4520da2b07640e2aa8e94384388",
+        inStock: 1,
+        isVeg: 1,
+        price: 7400,
+        variants: {},
+        variantsV2: {},
+        itemAttribute: {
+          vegClassifier: "VEG",
+        },
+        ribbon: {
+          text: "Bestseller",
+          textColor: "#ffffff",
+          topBackgroundColor: "#d53d4c",
+          bottomBackgroundColor: "#b02331",
+        },
+        showImage: true,
+        itemBadge: {},
+        badgesV2: {},
+        isBestseller: true,
+        ratings: {
+          aggregatedRating: {
+            rating: "4.7",
+            ratingCount: "277 ratings",
+            ratingCountV2: "277",
+          },
+        },
+      },
+      analytics: {},
+      hideRestaurantDetails: true,
+    },
+  },
+];
+
+module.exports = MenuItems;
